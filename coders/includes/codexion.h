96b030902e71d1ac99b34c54dbaef9089901dae6
SophiaLM <Sophiaalejandra.l.m@gmail.com>
2026-07-20 02:22:34 +0200

    Estructuras basicas&&Make

--- coders/includes/codexion.h
+++ coders/includes/codexion.h
@@ -4,6 +4,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+/* Errores */
+
 typedef enum e_error
 {
 	ERR_OK = 0,
@@ -14,6 +16,63 @@ typedef enum e_error
 	ERR_NEGATIVE,
 	ERR_SCHEDULER,
 }	t_error;
+
+/* scheduler */
+
+typedef enum e_scheduler
+{
+	SCHEDULER_FIFO,
+	SCHEDULER_EDF,
+}	t_scheduler;
+
+/* Los 8 argumentos validados */
+
+typedef struct s_config
+{
+	int			number_of_coders;
+	long long	time_to_burnout;
+	long long	time_to_compile;
+	long long	time_to_debug;
+	long long	time_to_refactor;
+	int			number_of_compiles_required;
+	long long	dongle_cooldown;
+	t_scheduler	scheduler;
+}	t_config;
+
+/* mutex, variable de condición, estado (libre/ocupado), * 
+ * timestamp `available_at` (para el cooldown), heap de peticiones*/
+
+typedef struct s_dongle
+{
+	int			id;
+	int			is_taken;
+	long long	available_at_ms;
+}	t_dongle;
+
+/* id, handle del hilo, `last_compile_start`, `compile_count` */
+
+typedef struct s_coder
+{
+	int			id;
+	long long	last_compile_start;
+	int			compile_count;
+	t_dongle	*first;
+	t_dongle	*second;
+	struct s_sim	*sim;
+}	t_coder;
+
+
+/* configuración, hora de inicio, array de coders, array de dongles, *
+ *  flag de parada + su mutex, mutex de impresión, hilo monitor. */
+
+typedef struct s_sim
+{
+	t_config	config;
+	long long	start_time_ms;
+	t_coder		*coders;
+	t_dongle	*dongles;
+	int			stop;
+}	t_sim;
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
